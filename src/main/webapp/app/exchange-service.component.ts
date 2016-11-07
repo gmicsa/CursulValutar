9213d24c937548fa2cbf536c0901629c5245fbcd
Serviceteam <rsteam@cegeka.be>
2016-11-07 18:10:45 +0200

    Create first ts for exchange service: module and component

---
+++ src/main/webapp/app/exchange-service.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+    selector: 'exchange-service'
+})
+export class ExchangeServiceComponent extends OnInit {
+    
+    
+    ngOnInit(): void {
+    }
+    
+}
