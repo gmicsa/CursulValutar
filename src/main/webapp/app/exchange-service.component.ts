1a6e14bade820f9685641b8811f4376072bc1471
Serviceteam <rsteam@cegeka.be>
2016-11-16 12:21:21 +0200

    Make a REST call into the rates service and adapt it into the exchange rates component

--- src/main/webapp/app/exchange-service.component.ts
+++ src/main/webapp/app/exchange-service.component.ts
@@ -1,12 +1,37 @@
 import {Component, OnInit} from "@angular/core";
+import {RatesService} from "./exchange-service.service";
+import {Rate} from "./rate";
 
 @Component({
-    selector: 'exchange-service'
+    selector: 'exchange-service',
+    template: `<h1>Hello</h1>
+        <div>What's up</div>
+    `,
+    providers: [RatesService]
 })
 export class ExchangeServiceComponent extends OnInit {
+    rates: Rate[];
+    error: any;
     
+    constructor(private ratesService : RatesService) {
+    }
     
     ngOnInit(): void {
+        this.retrieveRatesFromService();
+    }
+    
+    private retrieveRatesFromService(): void {
+        this.ratesService.retrieveRates().then(
+           rates => {
+               this.rates = rates;
+               this.error = null;
+           },
+            error => {
+                this.error = error;
+                this.rates = [];
+            }
+        )
+            
     }
     
 }
---
+++ src/main/webapp/app/exchange-service.service.ts
@@ -0,0 +1,30 @@
+/**
+ * Created by alexands on 16.11.2016.
+ */
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+import "rxjs/add/operator/toPromise";
+import {Rate} from "./rate";
+
+@Injectable()
+export class RatesService {
+    private urlGetRates = 'services/rates';
+    private urlDateAndCurrency = '16/11/16' + '/' + 'EUR';
+    
+    constructor(private http : Http) {
+    }
+
+    retrieveRates(): Promise<Rate[]> {
+        console.log('Retrieve rates is executing..');
+        return this.http.get(this.urlGetRates + '/' + this.urlDateAndCurrency)
+            .toPromise()
+            .then(response => response.json().map(item => new Rate(item)))
+            .catch(this.handleError);
+    }
+
+    private handleError(error: any): Promise<any> {
+        console.error('There was an error ', error);
+        return Promise.reject(error.message | error);
+    }
+
+}
