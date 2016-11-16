d6c80e4b44af252ee7a5bfc02fc169b960dc9fd8
Serviceteam <rsteam@cegeka.be>
2016-11-16 12:21:21 +0200

    Make a REST call into the rates service and adapt it into the exchange rates component

--- src/main/webapp/app/main.ts
+++ src/main/webapp/app/main.ts
@@ -1,1 +1,4 @@
 import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+import {ExchangeServiceModule} from "./exchange-service.module";
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(ExchangeServiceModule);
