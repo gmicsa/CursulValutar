1808ec8fcd74b67f0c4b14bf85c3968040b6a270
Serviceteam <rsteam@cegeka.be>
2016-12-02 13:48:22 +0200

    Keep angular2-datatable to see what the problem is with the import

--- src/main/webapp/app/exchange-service.module.ts
+++ src/main/webapp/app/exchange-service.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {HttpModule} from "@angular/http";
-import {DataTableModule} from "angular2-datatable";
+import {DataTableModule}  from "angular2-datatable";
 import {ExchangeServiceComponent} from "./exchange-service.component";
 
 @NgModule({
