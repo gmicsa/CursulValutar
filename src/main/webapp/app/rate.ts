a03f306815a845f12fbc6ed1279672009b5b72d8
Serviceteam <rsteam@cegeka.be>
2016-12-02 15:33:28 +0200

    Refactor aggregated rate and rate

--- src/main/webapp/app/rate.ts
+++ src/main/webapp/app/rate.ts
@@ -15,11 +15,20 @@ export class Rate {
         Object.assign(this, item);
     }
 
+    public static checkMatchingRates(buyRate:Rate, sellRate:Rate) {
+        if(Rate.haveDifferentBankNames(buyRate, sellRate)) {
+            throw new Error("Buy rate bank name does not match sell rate bank name");
+        }
+        if(buyRate.currencyType !== sellRate.currencyType) {
+            throw new Error("Buy rate currency type does not match sell rate currency type");
+        }
+    }
+
     public static haveSameBankName(rate1:Rate, rate2:Rate) {
         return rate1.bankName == rate2.bankName;
     }
 
-    static haveDifferentBankNames(rate1:Rate, rate2:Rate) {
+    private static haveDifferentBankNames(rate1:Rate, rate2:Rate) {
         return ! Rate.haveSameBankName(rate1, rate2);
     }
     
