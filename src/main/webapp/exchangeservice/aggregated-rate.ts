c13930114ad6c457b79034c3ba4dacbdcfa16f57
alexsch86 <schiaucualex@gmail.com>
2016-12-29 23:45:47 +0200

    Add CSS class attribute for buy and sell in aggregated rate

--- src/main/webapp/exchangeservice/aggregated-rate.ts
+++ src/main/webapp/exchangeservice/aggregated-rate.ts
@@ -8,14 +8,16 @@ export class AggregatedRate {
     bankName: string;
     currencyType: string;
     lastChangedAt: string;
-
+    
     buyValue: string;
     buyVariance: string;
     buyPercentVariance: string;
+    buyClassForCssLayout: string;
 
     sellValue: string;
     sellVariance: string;
     sellPercentVariance: string;
+    sellClassForCssLayout: string;
     
     constructor(buyRate: Rate, sellRate: Rate) {
         Rate.checkMatchingRates(buyRate, sellRate);
@@ -30,12 +32,14 @@ export class AggregatedRate {
     private computeAggregatedBuyRateValues(rate:Rate) {
         this.buyValue = rate.value.toFixed(4);
         this.buyVariance = ExchangeServiceUtils.convertNumberToSignedStringNumber(rate.evolution);
+        this.buyClassForCssLayout = rate.evolution >= 0 ? "greenSmall" : "redSmall";
         this.buyPercentVariance = ExchangeServiceUtils.convertNumberToSignedStringNumber(ExchangeServiceUtils.computeVariance(rate.value, rate.evolution));
     }
 
     private computeAggregatedSellRateValues(rate:Rate) {
         this.sellValue = rate.value.toFixed(4);
         this.sellVariance = ExchangeServiceUtils.convertNumberToSignedStringNumber(rate.evolution);
+        this.sellClassForCssLayout = rate.evolution >= 0 ? "greenSmall" : "redSmall";
         this.sellPercentVariance = ExchangeServiceUtils.convertNumberToSignedStringNumber(ExchangeServiceUtils.computeVariance(rate.value, rate.evolution));
     }
 
